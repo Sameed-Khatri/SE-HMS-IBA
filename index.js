33518bf36dcfe4da120c60ef00eb3f924faa7d93
Kunal-Kumar01 <kunal.lohano@gmail.com>
2023-12-04 11:40:42 +0500

    Connect Admin all Appointment to Frontend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const connection=require('./database');
+// const connection=require('./database');
 const userRoutes=require('./login');
 const adminRoutes=require('./admin');
 const cors = require('cors');
@@ -11,15 +11,15 @@ app.set('port', 3001);
 app.use(express.json());
 app.use(cors());
 
-app.use(async(req,res,next) => {
-    try {
-        req.db=await connection();
-        next();
-    } catch (error) {
-        console.error('Error establishing database connection:', error);
-        res.status(500).json({ status: 'Internal server error' });
-    }
-});
+// app.use(async(req,res,next) => {
+//     try {
+//         req.db=await connection();
+//         next();
+//     } catch (error) {
+//         console.error('Error establishing database connection:', error);
+//         res.status(500).json({ status: 'Internal server error' });
+//     }
+// });
 
 app.get('/MartinDow', async(req,res) =>{
     try {
